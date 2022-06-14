92e265ed4f8e754847181d98a9837a6ccd3a4cb8
Andrea <91068629+aestanciero@users.noreply.github.com>
2022-06-14 12:16:30 -0300

    Adivinanzas

---
+++ Adivinanzas/Adivinanzas/Adivinanzas/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Clear();
+// Genera un numero entero aleatorio de 1 a 20
+int numeroSecreto = new
+Random(DateTime.Now.Millisecond).Next(1, 21);
+
+Console.WriteLine("Ingresa un numero del 1 al 20");
+int miNumero = int.Parse(Console.ReadLine());
+
+
+var i = 1;
+
+while (miNumero != numeroSecreto)
+    {
+    i++;
+
+
+
+
+
+    if (miNumero > numeroSecreto)
+        {
+            Console.WriteLine("El numero elegido es mayor, intenta de nuevo");
+            Console.WriteLine("Ingresa un numero del 1 al 20");
+            miNumero = int.Parse(Console.ReadLine());
+
+        };
+        if (miNumero < numeroSecreto)
+        {
+            Console.WriteLine("El numero elegido es menor, intenta de nuevo");
+            Console.WriteLine("Ingresa un numero del 1 al 20");
+            miNumero = int.Parse(Console.ReadLine());
+        };
+
+
+   
+
+    if (miNumero == numeroSecreto)
+    {
+    
+
+            Console.WriteLine($"Felicitaciones acertaste el numero elegido era {numeroSecreto}, se realizo en {i} intentos");
+        }
+    
+    
+}
+    
+
+
+
