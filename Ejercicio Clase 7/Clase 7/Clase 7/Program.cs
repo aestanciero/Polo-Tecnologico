96ec207396f7616fa23afb2f191b89d7cf7afe6b
Andrea <91068629+aestanciero@users.noreply.github.com>
2022-06-14 12:14:40 -0300

    Clase 7

---
+++ Ejercicio Clase 7/Clase 7/Clase 7/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Clear();
+int[] numero = new int[10];
+
+
+for (int i = 0; i < 10; i++) 
+{
+    Console.WriteLine($"{i+1}-Por favor ingrese un numero:");
+    numero[i] = int.Parse(Console.ReadLine());
+
+}
+int contador=0;
+for (int i = 0; i < numero.Length; i++)
+    contador += numero[i]; 
+Console.WriteLine($"La suma de los numeros es { contador}");
+int promedio = contador / numero.Length;
+Console.WriteLine($"El promedio es : {promedio}");
+
+int numeroMayor = 0;
+
+ for(int i = 0; i < numero.Length; i++)
+{
+    if(numero[i] > numeroMayor)
+    {
+        numeroMayor=numero[i];
+      
+    }
+   
+    
+
+}
+
+int numeroMenor = numeroMayor;
+for (int i = 0; i < numero.Length; i++)
+{
+
+    if (numero[i] < numeroMenor)
+    {
+        numeroMenor = numero[i];
+    }
+}
+Console.WriteLine($"El numero mayor es {numeroMayor}");
+Console.WriteLine($"El numero menor es {numeroMenor}");
+
+for (int i = 0; i < numero.Length; i++)
+{
+    Console.WriteLine($"Numero ingresado {numero[i]}");
+}
+
+
+
+
+
+
+
